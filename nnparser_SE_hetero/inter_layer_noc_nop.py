fe5eb076da15c33554c109f264992066a8dcfee7
= <=>
2021-11-09 16:38:06 +0800

    handle some error

--- nnparser_SE_hetero/inter_layer_noc_nop.py
+++ nnparser_SE_hetero/inter_layer_noc_nop.py
@@ -104,28 +104,57 @@ def getInterLayerComm(dim_seq_1, dim_seq_2, network_param_1, parallel_1, network
 	chiplet_act_index_dict_single = {}
 	for q in range(parallel_2["Q"]):
 		for p in range(parallel_2["P"]):
+			p_index = P_output * p
+			q_index = Q_output * q
+			if p_index >= network_param_2["P"]:
+				break
 			chiplet_index = getChipletID(p,q,0,dim_mapping_seq_2,debug_flag)
 			offset_p = P_output * p *stride - padding
 			offset_q = Q_output * q *stride - padding
 			offset = [offset_p, offset_q]
 			chiplet_act_index_dict[chiplet_index] = offset
-	
+	#print("chiplet_act_index_dict")
+	#print(chiplet_act_index_dict)
 	#---记录前一层chiplet的output act分配情况
 	chiplet_act_index_dict_pre = {"P":{},"Q":{},"K":{}}
 	for p in range(parallel_1["P"]):
 		offset_p = P_i * p
-		chiplet_act_index_dict_pre["P"][p] = offset_p
-	chiplet_act_index_dict_pre["P"][parallel_1["P"]] = network_param_1["P"]
+		if offset_p < network_param_1["P"]:
+			chiplet_act_index_dict_pre["P"][p] = offset_p
+		else:
+			#chiplet_act_index_dict_pre["P"][p] = network_param_1["P"]
+			break
+	if p in chiplet_act_index_dict_pre["P"]:
+		chiplet_act_index_dict_pre["P"][p+1] = network_param_1["P"]
+	else:
+		chiplet_act_index_dict_pre["P"][p] = network_param_1["P"]
 
 	for q in range(parallel_1["Q"]):
 		offset_q = Q_i * q
 		chiplet_act_index_dict_pre["Q"][q] = offset_q
-	chiplet_act_index_dict_pre["Q"][parallel_1["Q"]] = network_param_1["Q"]
+		if offset_q < network_param_1["Q"]:
+			chiplet_act_index_dict_pre["Q"][q] = offset_q
+		else:
+			break
+	if q in chiplet_act_index_dict_pre["Q"]:
+		chiplet_act_index_dict_pre["Q"][q+1] = network_param_1["Q"]
+	else:
+		chiplet_act_index_dict_pre["Q"][q] = network_param_1["Q"]
 
 	for k in range(parallel_1["K"]):
 		offset_k = K_i * k
 		chiplet_act_index_dict_pre["K"][k] = offset_k
-	chiplet_act_index_dict_pre["K"][parallel_1["K"]] = network_param_1["K"]
+		if offset_k < network_param_1["K"]:
+			chiplet_act_index_dict_pre["K"][k] = offset_k
+		else:
+			break
+	if k in chiplet_act_index_dict_pre["K"]:
+		chiplet_act_index_dict_pre["K"][k+1] = network_param_1["K"]
+	else:
+		chiplet_act_index_dict_pre["K"][k] = network_param_1["K"]
+
+	#print("chiplet_act_index_dict_pre")
+	#print(chiplet_act_index_dict_pre)
 
 	#---记录当前chiplet与上一层chiplet的通信情况
 	#---comm_inter_layer_dict_o_i[layer(i+1)_chiplet_id] = {[layer(i)_chiplet_id]:comm_num,...}
@@ -140,10 +169,16 @@ def getInterLayerComm(dim_seq_1, dim_seq_2, network_param_1, parallel_1, network
 			if chip_id_i not in comm_inter_layer_dict_i_o:
 				comm_inter_layer_dict_i_o[chip_id_i] = {}
 			comm_inter_layer_dict_i_o[chip_id_i][chip_id] = comm_dict[chip_id_i]
-	
-	comm_dict = {}
+	#print("comm_inter_layer_dict_o_i")
+	#print(comm_inter_layer_dict_o_i)
+	#print("comm_inter_layer_dict_i_o")
+	#print(comm_inter_layer_dict_i_o)
+
+	comm_num_dict = {}
+	comm_type_dict = {"uni-cast":0, "multi-cast":0, "broadcast":0}
+	comm_type_times_dict = {"uni-cast":0, "multi-cast":0, "broadcast":0}
 	for chip_id_i in comm_inter_layer_dict_i_o:
-		comm_dict[chip_id_i] = {}
+		comm_num_dict[chip_id_i] = {}
 		num = 0
 		for chip_id_o in comm_inter_layer_dict_i_o[chip_id_i]:
 			chip_id_list = []
@@ -154,19 +189,71 @@ def getInterLayerComm(dim_seq_1, dim_seq_2, network_param_1, parallel_1, network
 				else:
 					if chip_id_o_ex != chip_id_i:
 						chip_id_list.append(chip_id_o_ex)
-			comm_dict[chip_id_i][num] = [comm_inter_layer_dict_i_o[chip_id_i][chip_id_o], chip_id_list]
-			num += 1
 
-	return comm_dict
+			commNum = comm_inter_layer_dict_i_o[chip_id_i][chip_id_o]
+			if len(chip_id_list) == 1:
+				comm_type_dict["uni-cast"] += commNum
+				comm_type_times_dict["uni-cast"] += 1
+			elif len(chip_id_list) + 1 < (parallel_2["P"]*parallel_2["Q"]*parallel_2["K"]) and len(chip_id_list) > 1:
+				comm_type_dict["multi-cast"] += commNum
+				comm_type_times_dict["multi-cast"] += 1
+			elif len(chip_id_list) + 1 == (parallel_2["P"]*parallel_2["Q"]*parallel_2["K"]):
+				comm_type_dict["broadcast"] += commNum
+				comm_type_times_dict["broadcast"] += 1
+			comm_num_dict[chip_id_i][num] = [commNum, chip_id_list]
+			num += 1
+	#print("comm_num_dict")
+	#print(comm_num_dict)
+	#print("comm_type_dict")
+	#print(comm_type_dict)
+	#print("comm_type_times_dict")
+	#print(comm_type_times_dict)
+
+	return comm_num_dict, comm_type_dict,comm_type_times_dict
 
 if __name__ == '__main__':
+
+	excel_datas = []
+
 	# 1表示前一层，2表示当前层
-	network_param_1 = {"P":64,"Q":64,"C":4,"K":8,"R":3,"S":3}
-	network_param_2 = {"P":32,"Q":32,"C":8,"K":16,"R":3,"S":3, "stride":2, "padding":1}
-	parallel_1 = {"P":4,"Q":4,"K":1}
-	parallel_2 = {"P":1,"Q":4,"K":4}
-	dim_seq_1 = ["Q","P","K"]
-	dim_seq_2 = ["P","Q","K"]
-	dict = getInterLayerComm(dim_seq_1, dim_seq_2, network_param_1, parallel_1, network_param_2, parallel_2, 0)
-	for id in dict:
-		print(id, " : ",dict[id])
+	network_param_1 = {"P":224,"Q":224,"C":3,"K":64,"R":3,"S":3}
+	network_param_2 = {"P":224,"Q":224,"C":64,"K":64,"R":3,"S":3, "stride":1, "padding":1}
+	
+	# parallel_type_1 = {"P":{"P":16,"Q":1,"K":1},'Q':{"P":1,"Q":16,"K":1},'K':{"P":1,"Q":1,"K":16},"PQ":{"P":4,"Q":4,"K":1},'PK':{"P":4,"Q":1,"K":4},'QK':{"P":1,"Q":4,"K":4}}
+	# parallel_type_2 = {"P":{"P":16,"Q":1,"K":1},'Q':{"P":1,"Q":16,"K":1},'K':{"P":1,"Q":1,"K":16},"PQ":{"P":4,"Q":4,"K":1},'PK':{"P":4,"Q":1,"K":4},'QK':{"P":1,"Q":4,"K":4}}
+	parallel_type_1 = {"P":{"P":16,"Q":1,"K":1},'K':{"P":1,"Q":1,"K":16},'PK':{"P":4,"Q":1,"K":4}}
+	parallel_type_2 = {"P":{"P":16,"Q":1,"K":1},'K':{"P":1,"Q":1,"K":16},'PK':{"P":4,"Q":1,"K":4}}
+
+	times = 0
+	for type2 in parallel_type_2:
+		for type1 in parallel_type_1:
+			parallel_1 = parallel_type_1[type1]
+			parallel_2 = parallel_type_2[type2]
+			dim_seq_1 = ["K","P","Q"]
+			dim_seq_2 = ["K","P","Q"]
+			print("----------- times = ", times)
+			print("parallel_1")
+			print(parallel_1)
+			print("parallel_2")
+			print(parallel_2)
+
+			dict1, type_dict1, type_dict2 = getInterLayerComm(dim_seq_1, dim_seq_2, network_param_1, parallel_1, network_param_2, parallel_2, 0)
+			
+			excel_datas.append([type1+"-"+type2, type1, type2, type_dict2["uni-cast"], type_dict2["multi-cast"], type_dict2["broadcast"], type_dict1["uni-cast"], type_dict1["multi-cast"], type_dict1["broadcast"] ])
+			print(dict1)
+			times += 1
+	
+	workbook = openpyxl.Workbook()
+	sheet = workbook.get_sheet_by_name('Sheet') 
+	# 写入标题
+	column_tite = ["parallel-type","parallel-type-i","parallel-type-i+1", "times-uni-cast","times-multi-cast","times-broadcast","num-uni-cast","num-multi-cast","num-broadcast"]
+	for col,column in enumerate(column_tite):
+		sheet.cell(1, col+1, column)
+	# 写入每一行
+	for row, data in enumerate(excel_datas):
+		for col, column_data in enumerate(data):
+			sheet.cell(row+2, col+1, column_data)
+
+	setname = str(network_param_1["P"]) + "_" + str(network_param_1["Q"]) + "_" + str(network_param_2["P"]) + "_" + str(network_param_2["Q"]) + "_" + str(network_param_2["K"]) + "_" + str(network_param_2["C"])
+
+	workbook.save("inter_layer_output_"+setname+".xls")
